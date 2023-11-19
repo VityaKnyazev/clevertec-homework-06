9242dfba3bb0b13ab69ce2fe5e60d11c48264b62
Vitya Knyazev <Utopio@tut.by>
2023-11-19 12:00:49 +0300

    Added tests for method's PersonDaoImpl save, update, delete.

--- src/main/java/ru/clevertec/knyazev/Main.java
+++ src/main/java/ru/clevertec/knyazev/Main.java
@@ -6,18 +6,23 @@ import ru.clevertec.knyazev.config.AppConfig;
 import ru.clevertec.knyazev.dao.PersonDAO;
 import ru.clevertec.knyazev.entity.Person;
 
+import java.util.UUID;
+
 public class Main {
 
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
         PersonDAO personDAOImpl = context.getBean("personDAOImpl", PersonDAO.class);
-        personDAOImpl.save(Person.builder()
+        Person savedPerson = personDAOImpl.save(Person.builder()
                 .name("Vano")
                 .surname("Patsuk")
                 .email("Vano@mail.ru")
                 .citizenship("Belarus")
                 .age(12)
                 .build());
+
+        savedPerson.setAge(14);
+        Person updatedPerson = personDAOImpl.update(savedPerson);
 
         System.out.printf("Hello %s%n", "spring context!");
     }
--- src/test/java/ru/clevertec/knyazev/dao/impl/PersonDAOImplTest.java
+++ src/test/java/ru/clevertec/knyazev/dao/impl/PersonDAOImplTest.java
@@ -1,24 +1,31 @@
 package ru.clevertec.knyazev.dao.impl;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+import org.assertj.core.api.Condition;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
+import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.KeyHolder;
+import ru.clevertec.knyazev.dao.exception.DAOException;
 import ru.clevertec.knyazev.entity.Person;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 @ExtendWith(MockitoExtension.class)
 public class PersonDAOImplTest {
+
+    @Captor
+    ArgumentCaptor<UUID> uuidArgumentCaptor;
 
     @Mock
     JdbcTemplate jdbcTemplateMock;
@@ -51,7 +58,7 @@ public class PersonDAOImplTest {
     @Test
     public void checkFindByIdShouldReturnOptionalEmpty() {
 
-        Mockito.when(jdbcTemplateMock.queryForObject(Mockito.anyString(), Mockito.any(RowMapper.class), Mockito.any()))
+        Mockito.when(jdbcTemplateMock.queryForObject(Mockito.anyString(), Mockito.any(RowMapper.class), Mockito.any(UUID.class)))
                 .thenThrow(new DataAccessException("Id not exists") {
                 });
 
@@ -105,7 +112,175 @@ public class PersonDAOImplTest {
         assertThat(actualPersons).isEmpty();
     }
 
+    @Test
+    public void checkSaveShouldReturnSavedPerson() {
+
+        UUID expectedSavedUUID = UUID.randomUUID();
+
+        Mockito.when(jdbcTemplateMock.update(Mockito.any(PreparedStatementCreator.class),
+                        Mockito.any(KeyHolder.class)))
+                .thenAnswer(invocation -> {
+                    KeyHolder keyHolder = invocation.getArgument(1);
+                    keyHolder.getKeyList()
+                            .add(new HashMap<>() {{
+                                put("id", expectedSavedUUID);
+                            }});
+                    return 1;
+                });
+
+        Person inputPerson = Person.builder()
+                .name("Misha")
+                .surname("Ivanov")
+                .email("misha@mail.ru")
+                .citizenship("Russia")
+                .age(34)
+                .build();
+
+        Person actualPerson = personDAOImpl.save(inputPerson);
+
+        assertThat(actualPerson).isNotNull()
+                .has(new Condition<Person>(p -> p.getId().equals(expectedSavedUUID),
+                        "saved UUID"));
+
+    }
+
+    @Test
+    public void checkSaveShouldThrowDAOException() {
+
+        Mockito.when(jdbcTemplateMock.update(Mockito.any(PreparedStatementCreator.class),
+                Mockito.any(KeyHolder.class))).thenThrow(new DataAccessException("Saving constraint") {});
+
+        Person inputPerson = Person.builder()
+                .name("Misha")
+                .surname("Ivanov")
+                .email("misha@mail.ru")
+                .citizenship("Russia")
+                .age(34)
+                .build();
+
+        assertThatExceptionOfType(DAOException.class)
+                .isThrownBy(() -> personDAOImpl.save(inputPerson));
+    }
+
+    @Test
+    public void checkSaveShouldThrowDAOExceptionWhenPersonIdIsNotNull() {
+
+        Person inputPerson = Person.builder()
+                .id(UUID.randomUUID())
+                .name("Misha")
+                .surname("Ivanov")
+                .email("misha@mail.ru")
+                .citizenship("Russia")
+                .age(34)
+                .build();
+
+        assertThatExceptionOfType(DAOException.class)
+                .isThrownBy(() -> personDAOImpl.save(inputPerson));
+    }
+
+    @Test
+    public void checkUpdateShouldReturnUpdatedPerson() {
+
+        Mockito.when(jdbcTemplateMock.update(Mockito.anyString(), Mockito.any(Object[].class)))
+                .thenReturn(1);
+
+        Person inputPerson = Person.builder()
+                .id(UUID.randomUUID())
+                .name("Misha")
+                .surname("Ivanov")
+                .email("misha@mail.ru")
+                .citizenship("Russia")
+                .age(34)
+                .build();
+
+        Person actualPerson = personDAOImpl.update(inputPerson);
+
+        assertThat(actualPerson).isEqualTo(inputPerson);
+    }
+
+    @Test
+    public void checkUpdateShouldThrowDAOException() {
+
+        Mockito.when(jdbcTemplateMock.update(Mockito.anyString(), Mockito.any(Object[].class)))
+                .thenThrow(new DataAccessException("Updating constraint") {});
+
+        Person inputPerson = Person.builder()
+                .id(UUID.randomUUID())
+                .name("Mimi")
+                .surname("Kot")
+                .email("mimisha@mail.ru")
+                .citizenship("Poland")
+                .age(28)
+                .build();
+
+        assertThatExceptionOfType(DAOException.class)
+                .isThrownBy(() -> personDAOImpl.update(inputPerson));
+    }
+
+    @Test
+    public void checkUpdateShouldThrowDAOExceptionWhenPersonIdIsNull() {
+
+        Person inputPerson = Person.builder()
+                .name("Mimi")
+                .surname("Kot")
+                .email("mimisha@mail.ru")
+                .citizenship("Poland")
+                .age(28)
+                .build();
+
+        assertThatExceptionOfType(DAOException.class)
+                .isThrownBy(() -> personDAOImpl.update(inputPerson));
+    }
+
+    @Test
+    public void checkDeleteShouldDeletePerson() {
+
+        Person expectedDeletingPerson = Person.builder()
+                .id(new UUID(24L, 18L))
+                .name("Misha")
+                .surname("Ivanov")
+                .email("misha@mail.ru")
+                .citizenship("Russia")
+                .age(34)
+                .build();
+
+        Mockito.when(jdbcTemplateMock.queryForObject(Mockito.anyString(), Mockito.any(RowMapper.class), Mockito.any()))
+                .thenReturn(expectedDeletingPerson);
+
+        Mockito.when(jdbcTemplateMock.update(Mockito.anyString(), Mockito.any(UUID.class)))
+                .thenReturn(1);
+
+        UUID inputPersonDeletingId = new UUID(24L, 18L);
+        personDAOImpl.delete(inputPersonDeletingId);
 
 
+        Mockito.verify(jdbcTemplateMock).update(Mockito.anyString(), uuidArgumentCaptor.capture());
+        assertThat(uuidArgumentCaptor.getValue())
+                .isEqualTo(expectedDeletingPerson.getId())
+                .isEqualTo(inputPersonDeletingId);
+    }
 
+    @Test
+    public void checkDeleteShouldThrowDAOException() {
+
+        Person expectedDeletingPerson = Person.builder()
+                .id(new UUID(15L, 118L))
+                .name("Misha")
+                .surname("Ivanov")
+                .email("misha@mail.ru")
+                .citizenship("Russia")
+                .age(26)
+                .build();
+
+        Mockito.when(jdbcTemplateMock.queryForObject(Mockito.anyString(), Mockito.any(RowMapper.class), Mockito.any()))
+                .thenReturn(expectedDeletingPerson);
+
+        Mockito.when(jdbcTemplateMock.update(Mockito.anyString(), Mockito.any(UUID.class)))
+                .thenThrow(new DataAccessException("Deleting constraint") {});
+
+        UUID deletingPersonUUID = new UUID(15L, 118L);
+
+        assertThatExceptionOfType(DAOException.class)
+                .isThrownBy(() -> personDAOImpl.delete(deletingPersonUUID));
+    }
 }
